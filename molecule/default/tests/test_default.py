553aa1e5dfd7249babf8f24cf322f5a3cb7cf5aa
trfore <trfore@users.noreply.github.com>
2025-05-20 12:03:23 -0500

    ci: add test for molecule config files

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,11 +1,25 @@
 import os
 import pytest
+import re
+from pathlib import Path
+
+mongodb_version = os.environ["MOLECULE_MONGODB_VERSION"]
+mongodb_version_maj = re.search(r"\d+", mongodb_version).group()
+
+
+def test_config_file(host):
+    if mongodb_version_maj >= "7":
+        filename = "default_mongo7+.conf"
+    else:
+        filename = "default_mongo6.conf"
+    expected = Path(__file__).with_name(filename)
+    assert expected.read_text() == host.file("/etc/mongod.conf").content_string
 
 
 def test_package_version(host):
     pkg = host.package("mongodb-org")
     assert pkg.is_installed
-    assert pkg.version == os.environ["MOLECULE_MONGODB_VERSION"]
+    assert pkg.version == mongodb_version
 
 
 def test_service_running_and_enabled(host):
--- molecule/thp/tests/test_thp.py
+++ molecule/thp/tests/test_thp.py
@@ -1,5 +1,11 @@
 import os
 import pytest
+from pathlib import Path
+
+
+def test_config_file(host):
+    expected = Path(__file__).with_name("expected_mongo.conf")
+    assert expected.read_text() == host.file("/etc/mongod.conf").content_string
 
 
 def test_package_version(host):
