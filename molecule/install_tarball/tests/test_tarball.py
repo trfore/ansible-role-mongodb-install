b1365fbabb70c9fc2947030d02510c62f63f697d
trfore <trfore@users.noreply.github.com>
2025-07-15 17:06:27 -0500

    refactor: drop support for installing via tarfile

--- molecule/install_tarball/tests/test_tarball.py
+++
@@ -1,9 +0,0 @@
-def test_service_running_and_enabled(host):
-    service = host.service("mongod")
-    assert service.is_running
-    assert service.is_enabled
-
-
-def test_service_tcp_port(host):
-    socket = host.socket("tcp://127.0.0.1:27017")
-    assert socket.is_listening
